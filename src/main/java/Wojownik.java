62f2ba93681eddad0b6f278bc7fa034e853ce023
D3vi11 <krystianb69@gmail.com>
2019-05-26 23:07:50 +0200

    3

--- src/main/java/Wojownik.java
+++ src/main/java/Wojownik.java
@@ -3,8 +3,13 @@ public class Wojownik extends Jednostka {
 
     public Wojownik(String name)
     {
-
-
+        setAtak(100);
+        setHp(1000);
+        setNazwa(name);
+        //setNr_pola();
+        setPancerz(100);
+        setPredkosc_ruchu(100);
+        setZasieg_ataku(1);
     }
 
     @Override
