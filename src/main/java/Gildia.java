cb48872f1a5175c5ed032c14e16e6cbda40e882b
D3vi11 <krystianb69@gmail.com>
2019-05-29 19:11:18 +0200

    Pole, Mapa zaaktualizowane

--- src/main/java/Gildia.java
+++ src/main/java/Gildia.java
@@ -1,11 +1,24 @@
+import java.util.Scanner;
+
 public class Gildia {
 
 
     public static void main(String [] args) {
-        System.out.println("tak"); //zmienilem napis "cos" na "tak'
+        Scanner scan = new Scanner(System.in);
+
+            for( ;; )
+            {
+                System.out.println("Wojna-gildii");
+                int x = scan.nextInt();
+                Mapa mapa = new Mapa(x);
+
+                        if(x==0){
+                            System.exit(0);
+                        }
+            }
     }
 
-    Jednostka [] tablica_jedn;
+    Jednostka [][] tablica_jedn;
 
     void ustaw_liczbe_gildii() {
     }
--- src/main/java/Mapa.java
+++ src/main/java/Mapa.java
@@ -1,7 +1,18 @@
 public class Mapa implements I_Mapa {
 
     private int rozmiar;
-    private Pole [] tablica_pol;
+    private Pole [][] tablica_pol;
+
+    Mapa(int rozmiar)
+    {
+        this.rozmiar=rozmiar;
+        tablica_pol= new Pole[rozmiar][rozmiar];
+        for(int i=0;i<rozmiar;i++)
+        for(int j=0;j<rozmiar;j++)
+        {
+            tablica_pol[i][j]= new Pole(i+1,j+1);
+        }
+    }
 
     @Override
     public boolean jesli_poza_mapa() {
