4fb5320e10dbcca3e94dd5409180c876aade097a
D3vi11 <krystianb69@gmail.com>
2019-05-29 21:59:01 +0200

    Poprawki

--- src/main/java/Wynik.java
+++ src/main/java/Wynik.java
@@ -1,9 +1,9 @@
 public class Wynik {
-    public static void zlicz_wojownik(int liczba_jednostek, Wojownik T[]){
+    public static void zlicz_wojownik(int liczba_jednostek, Wojownik [] T){
         int ilosc_zywych=liczba_jednostek;
         for(int i=1; i<=liczba_jednostek; i++)
         {
-            if(T[i].getCzy_żywy()) ilosc_zywych--;
+            if(!T[i].getCzy_żywy()) ilosc_zywych--;
         }
         System.out.println("Gildia wojownikow:"+ilosc_zywych);
         System.out.println("Ilosc zywych jednostek="+ilosc_zywych);
@@ -11,11 +11,11 @@ public class Wynik {
         System.out.println("Ilosc mertwych jednostek="+ilosc_martwych);
     }
 
-    public static void zlicz_lucznik(int liczba_jednostek, Lucznik T[]) {
+    public static void zlicz_lucznik(int liczba_jednostek, Lucznik [] T) {
         int ilosc_zywych=liczba_jednostek;
         for(int i=1; i<=liczba_jednostek; i++)
         {
-            if(T[i].getCzy_żywy()) ilosc_zywych--;
+            if(!T[i].getCzy_żywy()) ilosc_zywych--;
         }
         System.out.println("Gildia lucznikow:"+ilosc_zywych);
         System.out.println("Ilosc zywych jednostek="+ilosc_zywych);
@@ -23,11 +23,11 @@ public class Wynik {
         System.out.println("Ilosc mertwych jednostek="+ilosc_martwych);
     }
 
-    public static void zlicz_mag(int liczba_jednostek, Mag T[]) {
+    public static void zlicz_mag(int liczba_jednostek, Mag [] T) {
         int ilosc_zywych=liczba_jednostek;
         for(int i=1; i<=liczba_jednostek; i++)
         {
-            if(T[i].getCzy_żywy()) ilosc_zywych--;
+            if(!T[i].getCzy_żywy()) ilosc_zywych--;
         }
         System.out.println("Gildia magow:"+ilosc_zywych);
         System.out.println("Ilosc zywych jednostek="+ilosc_zywych);
