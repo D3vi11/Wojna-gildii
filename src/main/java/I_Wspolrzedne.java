0ea47738a56cb44f2aa9435eca781733f0177d0c
Filip <filippp1000@wp.pl>
2019-06-13 23:26:01 +0200

    Opisy- klasa I_wspolrzedne i wojownik.

--- src/main/java/I_Wspolrzedne.java
+++ src/main/java/I_Wspolrzedne.java
@@ -1,4 +1,18 @@
+/**
+ * \brief Interfejs zawierajacy metody uzywane w klasie Pole.
+ */
+
 public interface I_Wspolrzedne {
+
+    /**
+     * \brief Metoda zwraca wspolrzedna x.
+     * @return wspolrzedna x.
+     */
     int getWsp_x();
+
+    /**
+     * \brief Metoda zwraca wspolrzedna y.
+     * @return wspolrzedna y.
+     */
     int getWsp_y();
 }
--- src/main/java/Wojownik.java
+++ src/main/java/Wojownik.java
@@ -1,7 +1,22 @@
 import java.util.Random;
 
+/**
+ *  /brief Klasa jednostek z gildii wojownokow.
+ *
+ *  Klasa wojownik. Dzidziczy po klasie jednostka. Zwiera atrybuty i metody przypisane wowjownikowi.
+ */
+
 public class Wojownik extends Jednostka {
 
+    /**
+     * \brief Metoda nadpisujaca satystyki wojownika.
+     *
+     * Metoda nadpisuje satystyki wojownika o danym numerze na polu o dancyh wspolrzednych takie jak atak, punkty Hp, pancerz, obrazenia krytyczne modyfikator trafienia krytycznego. ustania równiez na początku stan wojownika na zywy.
+     *
+     * @param wsp_x parametr zawierajacy wspolrzedna x.
+     * @param wsp_y parametr zawierajacy wspolrzedna y.
+     * @param nr_jednostki parametr zawierajacy numer jednostki.
+     */
 
     Wojownik(int wsp_x,int wsp_y, int nr_jednostki)
     {
@@ -14,6 +29,14 @@ public class Wojownik extends Jednostka {
         setCzy_zywy(true);
     }
 
+    /**
+     * \brief Metoda okresla atak.
+     *
+     * Metoda ustala wielkosc ataku oraz obrazzenia krytyczne.
+     *
+     * @param wrog obiekt ktoremu zostanią odebrane punkty zdrowia
+     */
+
     @Override
     public void atak(Jednostka wrog) {
         if (losuj_kryt())
@@ -24,11 +47,32 @@ public class Wojownik extends Jednostka {
 
     }
 
+    /**
+     * \brief Metoda okresla obrazenia krytyczne.
+     *
+     * Metoda okresla wielkosc obrazen krytycznych wojownika oraz modyfikator trfien krytycznych.
+     *
+     * @param wrog obiekt na ktorym zostanie wykonana operacja zadania obrażeń krytycznych
+     * @param mod_kryt modyfikator trafienia krytycznego
+     */
+
     @Override
     public void kryt(Jednostka wrog,int mod_kryt) {
             wrog.setHp((wrog.getHp()-mod_kryt*getAtak())+wrog.getPancerz()/10);
     }
 
+    /**
+     *
+     * @return
+     */
+
+    /**
+     * \brief Metoda losujaca obrazenia krytyczne.
+     *
+     * Metoda w sposob losowy okresla czy obrazenia krytyczne maja wystapic u wojownika.
+     *
+     * @return czy uzyc obrazen krytycznych
+     */
     @Override
     public boolean losuj_kryt() {
         Random generator=new Random();
