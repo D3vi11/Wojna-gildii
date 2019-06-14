acd2fc57bf01fe7b780832e19fcaf89f85707d35
D3vi11 <krystianb69@gmail.com>
2019-06-14 13:10:32 +0200

    testy

---
+++ src/test/java/MagTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MagTest {
+
+    @Test
+    public void atak() {
+    }
+
+    @Test
+    public void kryt() {
+    }
+
+    @Test
+    public void losuj_kryt() {
+    }
+}
---
+++ src/test/java/WojownikTest.java
@@ -0,0 +1,18 @@
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class WojownikTest {
+
+    @Test
+    public void atak() {
+    }
+
+    @Test
+    public void kryt() {
+    }
+
+    @Test
+    public void losuj_kryt() {
+    }
+}
